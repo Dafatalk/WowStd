8473454a279f4809ad18e66c1428b7317b586d25
Dafatalk <dafa345.daf@gmail.com>
2023-05-15 13:45:29 -0500

    Arreglos pedidos finales del repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,72 @@
-import { Component, ElementRef } from '@angular/core';
+import { Component, ElementRef, OnInit  } from '@angular/core';
 
+
+function isElementInViewport(el:any) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'wowstudio';
-  
-  constructor(private elRef: ElementRef) { }
+  videomp4uri="assets/video.mp4"
+
+
+  currentIndex: number = 0; 
+
+  constructor(private elRef: ElementRef ) { 
+    
+  }
+
+
+  updateIndex(index: number) {
+    this.currentIndex = index;
+    if(this.currentIndex == 6){
+      const slider = this.elRef.nativeElement.querySelector('.slider');
+      slider.classList.add('pause');
+    }
+  }
+
+
+  ngOnInit() {
+    const videoElement = this.elRef.nativeElement.querySelector('#video');
+    const cuadElement = this.elRef.nativeElement.querySelector('#cuad');
+    const fqaElement = this.elRef.nativeElement.querySelector('#FQA');
+    const inteElement = this.elRef.nativeElement.querySelector('#inte');
+    const capaElement = this.elRef.nativeElement.querySelector('#capa');
+
+    window.addEventListener('scroll', function() {
+      if (isElementInViewport(videoElement)) {
+        // El elemento #video está visible en la ventana actual del navegador.
+      }
+
+      if (isElementInViewport(cuadElement)) {
+        // El elemento #cuad está visible en la ventana actual del navegador.
+      }
+
+      if (isElementInViewport(fqaElement)) {
+        // El elemento #FQA está visible en la ventana actual del navegador.
+      }
+
+      if (isElementInViewport(inteElement)) {
+        // El elemento #inte está visible en la ventana actual del navegador.
+      }
+
+      if (isElementInViewport(capaElement)) {
+        // El elemento #capa está visible en la ventana actual del navegador.
+      }
+    });
+  }
+
+
 
   scrollToStart() {
     const section = this.elRef.nativeElement.querySelector('#video');
@@ -22,6 +80,11 @@ export class AppComponent {
     const section = this.elRef.nativeElement.querySelector('#FQA');
     section.scrollIntoView({ behavior: 'smooth', block: 'start' });
   }
+  scrollToHab() {
+    const section = this.elRef.nativeElement.querySelector('#habita');
+    section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+  
 }
 
 
